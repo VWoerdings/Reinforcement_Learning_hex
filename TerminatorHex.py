90614cc736529e3d2bc6399bb6d5d1a766452e74
rwalen <61025173+rwalen@users.noreply.github.com>
2020-02-19 16:03:48 +0100

    terminator minimax

--- TerminatorHex.py
+++ TerminatorHex.py
@@ -1,26 +1,54 @@
-import copy
-
 class TerminatorHex:
     """This is a highly skilled hex game engine which will terminate opponent or terminate itself.
     """
 
-    def __init__(self, boardData):
+    def __init__(self, board, max_depth, heuristic_evaluator):
         """Initializes the Terminator AI for the HEX game
         Args:
-            board_size (int): Size of the hexagon grid.
+            board: the HexBoard class to which we bind
+            max_depth: max search depth
+            heuristic_evaluator: heuristic evaluation function (args hex_board, maximiser_color)
         """
-        self.board = boardData
+        self.board = board
+        self.max_depth = max_depth
+        self.heuristic_evaluator = heuristic_evaluator
 
-    def InitiateTerminator(self):
-        """initiating distruction
-        Args:
-            board_size (int): Size of the hexagon grid.
+    def terminator_move(self)
+        """initiating destruction
+        Return the best move according to TerminatorHex on self.board for the current color to move (blue_to_move on the board)
         """
-        return (2, 2)
+        return terminator_min_max(self.max_depth, 'max')
 
-    def terminateMinMax(self, depth):
-        if (depth == 0 or self.board.gameOver):
-            return
+    def terminator_min_max(self, depth, max_or_min):
+        moves = self.board.get_free_positions()
+        is_game_over = False
+        if (hex_board.check_win(hex_board.BLUE) or hex_board.check_win(hex_board.RED)):
+            is_game_over = True
+        if (depth == 0 or is_game_over or len(moves) == 0):
+            print("@TerminatorHex.terminator_min_max: no valid moves left, or out of depth")
+            return None # shouldn't happen
+        elif (max_or_min == 'max'): # maximise
+            value = float('-inf')
+            best_move = None
+            for move in moves:
+                deepened_board = copy.deepcopy(hex_board)
+                deepened_board.set_position_auto(move)
+                new_value = minimax(deepened_board, depth - 1, 'min', self.heuristic_evaluator)
+                if (new_value > value):
+                    value = new_value
+                    best_move = move
+            return best_move
+        elif (max_or_min == 'min'): # minimise
+            value = float('inf')
+            best_move = None
+            for move in moves:
+                deepened_board = copy.deepcopy(hex_board)
+                deepened_board.set_position_auto(move)
+                new_value = minimax(deepened_board, depth - 1, 'max', self.heuristic_evaluator)
+                if (new_value < value):
+                    value = new_value
+                    best_move = move
+            return best_move
     
 def minimax(hex_board, depth, max_or_min, evaluator): # 'HexBoard.BLUE', 'HexBoard.RED' for color; 'min' or 'max' to call
     """The minimax algorithm on the HexBoard
@@ -30,7 +58,8 @@ def minimax(hex_board, depth, max_or_min, evaluator): # 'HexBoard.BLUE', 'HexBoa
             max_or_min: maximise or minimise the current color to play (blue_to_move)
             evaluator: evaluator function. Called with args hex_board, maximiser_color
         Returns:
-            maximised/minimised value according to the evaluator"""    
+            maximised/minimised value according to the evaluator"""
+            
     moves = hex_board.get_free_positions()
     maximiser_color = [hex_board.BLUE, hex_board.RED][(max_or_min == 'max') ^ (hex_board.blue_to_move)]
     is_game_over = False
