2d69c006100f9ebcd3616edb96175ff16bae4e98
Abhishek <akanth1994@gmail.com>
2020-04-15 22:23:43 +0200

    Basic mountain car and need to add q table

--- A3/GymAI.py
+++ A3/GymAI.py
@@ -4,46 +4,48 @@ Created on Mon Apr 13 16:44:52 2020
 
 @author: Abhishek
 """
-import gym
 import random
 import numpy as np
 from keras.models     import Sequential
 from keras.layers     import Dense
 from keras.optimizers import Adam
 
+# https://medium.com/@ts1829/solving-mountain-car-with-q-learning-b77bf71b1de2
+
 class GymAI:
 
-    def __init__(self, game_env, episode, score_required):
+    def __init__(self, game_env, episode, required_score, steps, number_states):
         self.env = game_env
         self.episode = episode
-        self.iteration = 200
+        self.required_score = required_score
+        self.steps = steps
         self.game_memory = []
-        self.previous_observation =[]
-        self.training_data = []
-        self.score_required = score_required
+        self.previous_observation = []
         self.accepted_scores = []
+        self.training_data = []
+        self.env.seed(0)
+        np.random.seed(0)
         self.env.reset()
         
-    def get_game_model(self):
+    def data_preparation(self):
+        self.game_memory = []
+        self.previous_observation = []
         self.accepted_scores = []
+        self.training_data = []
         for game_index in range(self.episode):
-            print(game_index)
             score = 0
-            self.game_memory = []
-            self.previous_observation = []
-            for step_index in range(self.iteration):
+            for index in range(self.steps):
                 action = random.randrange(0, self.env.action_space.n)
                 observation, reward, done, info = self.env.step(action)
-                
                 if len(self.previous_observation) > 0:
                     self.game_memory.append([self.previous_observation, action])
-                    
                 self.previous_observation = observation
+                if observation[0] > -0.2:
+                    reward = 1
                 score += reward
                 if done:
                     break
-                
-            if score >= self.score_required:
+            if score >= self.required_score:
                 self.accepted_scores.append(score)
                 for data in self.game_memory:
                     if data[1] == 1:
@@ -53,22 +55,22 @@ class GymAI:
                     elif data[1] == 2:
                         output = [0, 0, 1]
                     self.training_data.append([data[0], output])
-            
             self.env.reset()
         print(self.accepted_scores)
-        X = np.array([i[0] for i in self.training_data]).reshape(-1, len(self.training_data[0][0]))
-        y = np.array([i[1] for i in self.training_data]).reshape(-1, len(self.training_data[0][1]))
-        model = self.build_model(input_size=len(X[0]), output_size=len(y[0]))    
-        model.fit(X, y, epochs=5)
-        print(model)
-        return model
+        return self.training_data
     
-    def build_model(self,input_size, output_size):
+    def build_model(self, input_size, output_size):
         model = Sequential()
-        model.add(Dense(150, input_dim=input_size, activation='relu'))
-        model.add(Dense(50, activation='relu'))
+        model.add(Dense(128, input_dim=input_size, activation='relu'))
+        model.add(Dense(52, activation='relu'))
         model.add(Dense(output_size, activation='linear'))
         model.compile(loss='mse', optimizer=Adam())
         return model
-
-        
+    
+    def train_model(self, data):
+        X = np.array([i[0] for i in data]).reshape(-1, len(data[0][0]))
+        y = np.array([i[1] for i in data]).reshape(-1, len(data[0][1]))
+        model = self.build_model(len(X[0]),len(y[0]))
+        model.fit(X, y, epochs=5)
+        return model
+    
--- A3/MountaincarRL.py
+++ A3/MountaincarRL.py
@@ -4,37 +4,41 @@ Created on Mon Apr 13 17:02:34 2020
 
 @author: Abhishek
 """
-
+import gym
 from GymAI import *
 
-env = gym.make("MountainCar")
+env = gym.make("MountainCar-v0")
 episode = 1000
-required_Score = -198
+required_score = -198
+steps = 200
+number_states = 40
 
-gymAI = GymAI(env, episode, required_Score)
-model = gymAI.get_game_model()
-
-env.reset()
+gymAI = GymAI(env, episode, required_score, steps, number_states)
+train_data = gymAI.data_preparation()
+model = gymAI.train_model(train_data)
 
 scores = []
 choices = []
 for each_game in range(100):
     score = 0
+    game_memory = []
     prev_obs = []
-    for step_index in range(1000):
+    for step_index in range(steps):
         env.render()
         if len(prev_obs)==0:
-            action = random.randrange(0, env.action_space.n)
+            action = random.randrange(0,2)
         else:
             action = np.argmax(model.predict(prev_obs.reshape(-1, len(prev_obs)))[0])
+        
         choices.append(action)
         new_observation, reward, done, info = env.step(action)
         prev_obs = new_observation
-        score+=reward
+        game_memory.append([new_observation, action])
+        score += reward
         if done:
             break
-    env.reset()
-    scores.append(score)
-
-print('Average Score:', sum(scores)/len(scores))
-print('choice 1:{}  choice 0:{}'.format(choices.count(1)/len(choices),choices.count(0)/len(choices)))
+env.reset()
+scores.append(score)
+print(scores)
+print('Average Score:',sum(scores)/len(scores))
+print('choice 1:{}  choice 0:{} choice 2:{}'.format(choices.count(1)/len(choices),choices.count(0)/len(choices),choices.count(2)/len(choices)))
